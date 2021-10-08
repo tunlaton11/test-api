a6d0ecd26702b3456d19db2f1873b6819b31f890
tunlaton <tunlaton11@gmail.com>
2021-10-09 00:20:03 +0700

    test

--- project1/settings.py
+++ project1/settings.py
@@ -127,5 +127,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [BASE_DIR / "static",'/static',]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles')
 django_heroku.settings(locals())
